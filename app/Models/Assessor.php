ceee2603b87dd8498a1685c3629714e3ac2f09f5
kunga <kungapatrick81@gmail.com>
2025-02-18 11:30:02 +0300

    add student, assessor, assessment models

---
+++ app/Models/Assessor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+//this model represents the assessor
+//tailored to have the details from the user model
+class Assessor extends Model
+{
+    //
+    protected $fillable = [
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function student(){
+        return $this->belongsToMany(Student::class);
+    }
+
+    public function assessment(){
+        return $this->hasMany(Assessment::class);
+    }
+}
