2afeb2e48832f10ff25b12d078cec71dfcc9d956
kunga <kungapatrick81@gmail.com>
2025-02-19 23:47:05 +0300

    upadte assessor migration

--- database/migrations/2025_02_16_095851_create_assessors_table.php
+++ database/migrations/2025_02_16_095851_create_assessors_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('assessors', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('employee_id')->nullable(false);
             $table->timestamps();
         });
     }
