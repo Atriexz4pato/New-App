676371a6d80a4009edcaa15e993d3578fe29dfb1
kunga <kungapatrick81@gmail.com>
2025-02-19 23:48:08 +0300

    upadte admin controller to add new assessor

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,5 +12,41 @@ class AdminController extends Controller
     public function dashboard()
     {
         return view('admin.dashboard');
+
     }
+        //this method is used to create a new assessor in the system
+         public function createAssessor(){
+
+             return view('admin.createAssessor');
+
+
+        }
+
+        public function storeAssessor(Request $request){
+            $data = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:assessors',
+                'employee_id' => 'required|string|max:255|unique:assessors',
+                'department' => 'required|string|max:255',
+                'phone_number' => 'required|string|max:255',
+            ]);
+
+            //persist the assessor details to  users table
+            $user= User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'department' => $data['department'],
+                'phone_number' => $data['phone_number'],
+
+
+            ]);
+            //persist the assessor extra details to the assessor table
+            $user->assessor()->create([
+                'user_id' => $user->id,
+                'employee_id' => $data['employee_id'],
+            ]);
+            return redirect()->route('admin.dashboard')->with('success','Assessor added successfully');
+
+
+        }
 }
